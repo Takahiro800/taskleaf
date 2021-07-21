b2aa265a575687b36a8fb681e94ff7e333aaee4e
Takahiro Abe <hirotaka081630@gmail.com>
2021-07-21 20:41:03 +0900

    test: capybaraの導入

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -5,6 +5,7 @@ describe 'タスク管理機能', type: :system do
 		before do
 			# ユーザーAを作成しておく
 			user_a = FactoryBot.create(:admin_user, name: 'ユーザーA', email: 'a@example.com')
+
 			# 作成者がユーザーAであるタスクを作成しておく
 			FactoryBot.create(:task, name: '最初のタスク', user: user_a)
 		end
@@ -12,10 +13,15 @@ describe 'タスク管理機能', type: :system do
 		context 'ユーザーAがログインしているとき' do
 			before do
 				# ユーザーAでログインする
+				visit login_path
+				fill_in 'メールアドレス', with: 'a@example.com'
+				fill_in 'パスワード', with: 'password'
+				click_button 'ログインする'
 			end
 
 			it 'ユーザーAが作成したタスクが表示される' do
 				# 作成済みのタスクの名称が画面上に表示されていることを確認
+				expect(page).to have_content '最初のタスク'
 			end
 		end
 	end
